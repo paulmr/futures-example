49dde7db5599388a457a451777bca9ea9afc1a38
Paul Roberts <paul.roberts@guardian.co.uk>
2020-03-03 12:41:25 +0000

    set up for simple threadable stuff

--- src/main/scala/FuturesExample.scala
+++ src/main/scala/FuturesExample.scala
@@ -1,5 +1,30 @@
 package futures_example
 
+import scala.util.Random
+import scala.concurrent.{ Future, ExecutionContext }
+import java.util.concurrent.Executors
+
 object FuturesExample {
-  def main(args: Array[String]) = ()
+
+  val singleExecutorContext = {
+    val es = Executors.newSingleThreadExecutor()
+    ExecutionContext.fromExecutorService(es)
+  }
+
+  val delay = 1000
+
+  def slowNumber: Int = {
+    Thread.sleep(delay)
+    val res = Random.nextInt
+    println(s"random: $res")
+    res
+  }
+
+  def slowNumberF: Future[Int] = ???
+
+  def slowNumbersF(n: Int): Future[Seq[Int]] = ???
+
+  def main(args: Array[String]) = {
+    println(List.fill(10)(slowNumber).mkString(", "))
+  }
 }
